45caa44ecbfb9f2ea86660281bb01ca84088121d
abraraboelmaaty <abraraboelmaaty24@gmail.com>
2022-03-27 23:36:15 +0200

    changes on clinic & medicine

--- Model/Medicine.js
+++ Model/Medicine.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const medicineSchema = new mongoose.Schema({
+    name:{type:String},
     productionDate: {type:Date,default:Date.now()},
     expirationDate: {type: Date,default:Date.now()},
     price: {type: Number},
